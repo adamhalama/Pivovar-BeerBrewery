bf32b8101198251fd1f078b4b976a14d70795383
hadamhej <47830178+hadamhej@users.noreply.github.com>
2019-02-23 23:14:41 +0100

    Update pivoprogram1.c
    pridane dalsie whiles (t2,t3) pridane odpocitavanie od tx

--- pivoprogram1.c
+++ pivoprogram1.c
@@ -8,8 +8,8 @@
 #define LedPin3 3
 #define ButtonPin 4
 
-//#define BuzzPin 5
-#define BuzzPin 1
+#define BuzzPin 5
+//#define BuzzPin 1
 
 
 #include <lcd.h>
@@ -51,12 +51,32 @@ int main(void)
     int starttime,nowtime;
     float dlzkavaru;
     int etapaStarttime;
+    int vysladzStarttime;
 
     int inputSecond;
     int hours1,minutes1,seconds1;
     int remainingSeconds;
     int secondsInHour = 60 * 60;
     int secondsInMinute = 60;
+
+    //tx teplota v stupnoch C
+    //cx cas v minutach
+
+
+    int t1=24, c1=1  ;
+    int t2=26, c2=1  ;
+    int t3=27, c3=1  ;
+    //int t4=, c4=  ;
+    //int t5=, c5=  ;
+    //int t6=, c6=  ;
+    //int t7=, c7=  ;
+    //int t8=, c8=  ;
+    //int t9=, c9=  ;
+
+    int tx=c1+c2+c3;
+    int txHlas=tx-2;
+    int txi=1;
+
 
         	if(wiringPiSetup() == -1){ //when initialize wiring failed,print messageto screen
             printf("setup wiringPi failed !");
@@ -93,7 +113,7 @@ int main(void)
 
 
 
-	while(x == 1){ //while 1
+	while(x == 1){ //while 1 stupanie na zaciatocnu teplotu t1=======================================================================================================================
 
 			fd = open("/sys/bus/w1/devices/28-020f9177242c/w1_slave", O_RDONLY);
 
@@ -132,13 +152,13 @@ int main(void)
     lcdPosition(lcd, 10, 0);
     lcdPrintf(lcd, "%.1f C", temp);
 
-    if(temp<=24.0){
+    if(temp<=t1){
     digitalWrite(LedPin0, LOW); //led on
             delay(1);
 
     }
 
-    else if(temp>24.0){
+    else if(temp>t1){
 
     digitalWrite(LedPin0, HIGH); //led off
     delay(100);
@@ -148,7 +168,7 @@ int main(void)
 } //koniec while 1
 
 x=1;
-    while(x == 1){ //while 2
+    while(x == 1){ //while 2 potvdenie pridania sladu=======================================================================================================================
 
 
 
@@ -287,16 +307,16 @@ delay(100);
 
 } //koniec while 2
 
-
     lcdPosition(lcd, 0, 0);             //premazanie displaya
     lcdPrintf(lcd, "                ");
     lcdPosition(lcd, 0, 1);
     lcdPrintf(lcd, "                ");
 
-x=1;
 seconds = time(NULL);
+vysladzStarttime=seconds;
 etapaStarttime=seconds;
-    while(x == 1){ //while 3
+x=1;
+    while(x == 1){ //while 3 t1=======================================================================================================================
 
 			fd = open("/sys/bus/w1/devices/28-020f9177242c/w1_slave", O_RDONLY);
 
@@ -334,16 +354,343 @@ etapaStarttime=seconds;
     lcdPosition(lcd, 10, 0);
     lcdPrintf(lcd, "%.1f C", temp);
 
-    if(temp<=24.0){
+    if(temp<=t1){
     digitalWrite(LedPin0, LOW); //led on
             delay(100);
     }
 
-    else if(temp>24.0){
+    else if(temp>t1){
 
     digitalWrite(LedPin0, HIGH); //led off
     delay(100);
     }
+
+
+if(txi == 1){
+   //cas
+   seconds = time(NULL);
+    printf("aktualne sekundy: %ld\n", seconds);
+    nowtime=seconds;
+
+    dlzkavaru=nowtime-vysladzStarttime;
+
+
+
+  inputSecond=dlzkavaru;
+
+  hours1 = (inputSecond/secondsInHour);
+
+  remainingSeconds = inputSecond - (hours1 * secondsInHour);
+  minutes1 = remainingSeconds/secondsInMinute;
+
+  remainingSeconds = remainingSeconds - (minutes1*secondsInMinute);
+  seconds1 = remainingSeconds;
+  dlzkavaru=dlzkavaru/60;
+
+printf("\n %.3f \n", dlzkavaru);
+printf("\n %d \n", txHlas);
+  if (dlzkavaru>=txHlas){
+
+    lcdPosition(lcd, 0, 1);
+    lcdPrintf(lcd, "Vysladz. voda");
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+sleep(1);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+
+txi=0;
+
+
+  }
+}
+
+
+if(1 == 1){
+   //cas
+   seconds = time(NULL);
+    printf("aktualne sekundy: %ld\n", seconds);
+    nowtime=seconds;
+
+    dlzkavaru=nowtime-etapaStarttime;
+
+
+  inputSecond=dlzkavaru;
+
+  hours1 = (inputSecond/secondsInHour);
+
+  remainingSeconds = inputSecond - (hours1 * secondsInHour);
+  minutes1 = remainingSeconds/secondsInMinute;
+
+  remainingSeconds = remainingSeconds - (minutes1*secondsInMinute);
+  seconds1 = remainingSeconds;
+
+  printf("cas od zaciatku t1:  %d:%d:%d\n",hours1,minutes1,seconds1);
+}
+
+
+lcdPosition(lcd, 0, 0);
+lcdPrintf(lcd, "%d:%d:%d",hours1,minutes1,seconds1);
+
+//dlzkavaru v minutach
+dlzkavaru=dlzkavaru/60;
+
+if (dlzkavaru>=c1){
+	x=0;
+	}
+
+
+
+} //koniec while 3
+
+    lcdPosition(lcd, 0, 0);             //premazanie displaya
+    lcdPrintf(lcd, "                ");
+    lcdPosition(lcd, 0, 1);
+    lcdPrintf(lcd, "                ");
+
+x=1;
+    while(x == 1){ //while 4 stupanie na t2=======================================================================================================================
+
+			fd = open("/sys/bus/w1/devices/28-020f9177242c/w1_slave", O_RDONLY);
+
+
+		if(-1 == fd){
+			perror("open device file error");
+			return 1;
+		}
+
+		while(1){
+			ret = read(fd, buf, BUFSIZE);
+			if(0 == ret){
+				break;
+			}
+			if(-1 == ret){
+				if(errno == EINTR){
+					continue;
+				}
+				perror("read()");
+				close(fd);
+				return 1;
+			}}
+
+		for(i=0;i<sizeof(buf);i++){
+			if(buf[i] == 't'){
+				for(j=0;j<sizeof(tempBuf);j++){
+					tempBuf[j] = buf[i+2+j];
+				}}}
+
+		temp = (float)atoi(tempBuf) /1000 ;
+            printf("%.3f C\n",temp);
+		close(fd);
+
+    if(txi == 1){
+   //cas
+   seconds = time(NULL);
+    printf("aktualne sekundy: %ld\n", seconds);
+    nowtime=seconds;
+
+    dlzkavaru=nowtime-vysladzStarttime;
+
+
+
+  inputSecond=dlzkavaru;
+
+  hours1 = (inputSecond/secondsInHour);
+
+  remainingSeconds = inputSecond - (hours1 * secondsInHour);
+  minutes1 = remainingSeconds/secondsInMinute;
+
+  remainingSeconds = remainingSeconds - (minutes1*secondsInMinute);
+  seconds1 = remainingSeconds;
+  dlzkavaru=dlzkavaru/60;
+
+printf("\n %.3f \n", dlzkavaru);
+printf("\n %d \n", txHlas);
+  if (dlzkavaru>=txHlas){
+
+    lcdPosition(lcd, 0, 1);
+    lcdPrintf(lcd, "Vysladz. voda");
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+sleep(1);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+
+txi=0;
+
+
+  }
+}
+
+
+    lcdPosition(lcd, 10, 0);
+    lcdPrintf(lcd, "%.1f C", temp);
+
+    if(temp<=t2){
+    digitalWrite(LedPin0, LOW); //led on
+            delay(1);
+
+    }
+
+    else if(temp>t2){
+
+    digitalWrite(LedPin0, HIGH); //led off
+    delay(100);
+
+        x=0;
+    }
+} //koniec while 4
+
+    lcdPosition(lcd, 0, 0);             //premazanie displaya
+    lcdPrintf(lcd, "                ");
+    lcdPosition(lcd, 0, 1);
+    lcdPrintf(lcd, "                ");
+
+seconds = time(NULL);
+etapaStarttime=seconds;
+x=1;
+    while(x == 1){ //while 5 t2=======================================================================================================================
+
+			fd = open("/sys/bus/w1/devices/28-020f9177242c/w1_slave", O_RDONLY);
+
+		if(-1 == fd){
+			perror("open device file error");
+			return 1;
+		}
+
+		while(1){
+			ret = read(fd, buf, BUFSIZE);
+			if(0 == ret){
+				break;
+			}
+			if(-1 == ret){
+				if(errno == EINTR){
+					continue;
+				}
+				perror("read()");
+				close(fd);
+				return 1;
+			}}
+
+		for(i=0;i<sizeof(buf);i++){
+			if(buf[i] == 't'){
+				for(j=0;j<sizeof(tempBuf);j++){
+					tempBuf[j] = buf[i+2+j];
+				}}}
+
+		temp = (float)atoi(tempBuf) /1000 ;
+            printf("%.3f C\n",temp);
+		close(fd);
+
+
+
+    lcdPosition(lcd, 10, 0);
+    lcdPrintf(lcd, "%.1f C", temp);
+
+    if(temp<=t2){
+    digitalWrite(LedPin0, LOW); //led on
+            delay(100);
+    }
+
+    else if(temp>t2){
+
+    digitalWrite(LedPin0, HIGH); //led off
+    delay(100);
+    }
+
+    if(txi == 1){
+   //cas
+   seconds = time(NULL);
+    printf("aktualne sekundy: %ld\n", seconds);
+    nowtime=seconds;
+
+    dlzkavaru=nowtime-vysladzStarttime;
+
+
+
+  inputSecond=dlzkavaru;
+
+  hours1 = (inputSecond/secondsInHour);
+
+  remainingSeconds = inputSecond - (hours1 * secondsInHour);
+  minutes1 = remainingSeconds/secondsInMinute;
+
+  remainingSeconds = remainingSeconds - (minutes1*secondsInMinute);
+  seconds1 = remainingSeconds;
+  dlzkavaru=dlzkavaru/60;
+
+printf("\n %.3f \n", dlzkavaru);
+printf("\n %d \n", txHlas);
+  if (dlzkavaru>=txHlas){
+
+    lcdPosition(lcd, 0, 1);
+    lcdPrintf(lcd, "Vysladz. voda");
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+sleep(1);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+
+txi=0;
+
+
+  }
+}
+
 
    //cas
    seconds = time(NULL);
@@ -363,7 +710,7 @@ etapaStarttime=seconds;
   remainingSeconds = remainingSeconds - (minutes1*secondsInMinute);
   seconds1 = remainingSeconds;
 
-  printf("cas od zaciatku etapy:  %d:%d:%d\n",hours1,minutes1,seconds1);
+  printf("cas od zaciatku t1:  %d:%d:%d\n",hours1,minutes1,seconds1);
 
 
 
@@ -373,14 +720,285 @@ lcdPrintf(lcd, "%d:%d:%d",hours1,minutes1,seconds1);
 //dlzkavaru v minutach
 dlzkavaru=dlzkavaru/60;
 
-if (1<=dlzkavaru){
+
+
+if (dlzkavaru>=c2){
 	x=0;
 	}
 
 
 
-} //koniec while 3
+} //koniec while 5
+
+    lcdPosition(lcd, 0, 0);             //premazanie displaya
+    lcdPrintf(lcd, "                ");
+    lcdPosition(lcd, 0, 1);
+    lcdPrintf(lcd, "                ");
+
+x=1;
+    while(x == 1){ //while 6 stupanie na t3=======================================================================================================================
+
+			fd = open("/sys/bus/w1/devices/28-020f9177242c/w1_slave", O_RDONLY);
+
+
+		if(-1 == fd){
+			perror("open device file error");
+			return 1;
+		}
+
+		while(1){
+			ret = read(fd, buf, BUFSIZE);
+			if(0 == ret){
+				break;
+			}
+			if(-1 == ret){
+				if(errno == EINTR){
+					continue;
+				}
+				perror("read()");
+				close(fd);
+				return 1;
+			}}
+
+		for(i=0;i<sizeof(buf);i++){
+			if(buf[i] == 't'){
+				for(j=0;j<sizeof(tempBuf);j++){
+					tempBuf[j] = buf[i+2+j];
+				}}}
+
+		temp = (float)atoi(tempBuf) /1000 ;
+            printf("%.3f C\n",temp);
+		close(fd);
 
 
 
+    lcdPosition(lcd, 10, 0);
+    lcdPrintf(lcd, "%.1f C", temp);
+
+    if(txi == 1){
+   //cas
+   seconds = time(NULL);
+    printf("aktualne sekundy: %ld\n", seconds);
+    nowtime=seconds;
+
+    dlzkavaru=nowtime-vysladzStarttime;
+
+
+
+  inputSecond=dlzkavaru;
+
+  hours1 = (inputSecond/secondsInHour);
+
+  remainingSeconds = inputSecond - (hours1 * secondsInHour);
+  minutes1 = remainingSeconds/secondsInMinute;
+
+  remainingSeconds = remainingSeconds - (minutes1*secondsInMinute);
+  seconds1 = remainingSeconds;
+  dlzkavaru=dlzkavaru/60;
+
+printf("\n %.3f \n", dlzkavaru);
+printf("\n %d \n", txHlas);
+  if (dlzkavaru>=txHlas){
+
+    lcdPosition(lcd, 0, 1);
+    lcdPrintf(lcd, "Vysladz. voda");
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+sleep(1);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+
+txi=0;
+
+
+  }
 }
+
+
+
+    if(temp<=t3){
+    digitalWrite(LedPin0, LOW); //led on
+            delay(1);
+
+    }
+
+    else if(temp>t3){
+
+    digitalWrite(LedPin0, HIGH); //led off
+    delay(100);
+
+        x=0;
+    }
+} //koniec while 6
+
+    lcdPosition(lcd, 0, 0);             //premazanie displaya
+    lcdPrintf(lcd, "                ");
+    lcdPosition(lcd, 0, 1);
+    lcdPrintf(lcd, "                ");
+
+seconds = time(NULL);
+etapaStarttime=seconds;
+x=1;
+    while(x == 1){ //while 7 t3=======================================================================================================================
+
+			fd = open("/sys/bus/w1/devices/28-020f9177242c/w1_slave", O_RDONLY);
+
+		if(-1 == fd){
+			perror("open device file error");
+			return 1;
+		}
+
+		while(1){
+			ret = read(fd, buf, BUFSIZE);
+			if(0 == ret){
+				break;
+			}
+			if(-1 == ret){
+				if(errno == EINTR){
+					continue;
+				}
+				perror("read()");
+				close(fd);
+				return 1;
+			}}
+
+		for(i=0;i<sizeof(buf);i++){
+			if(buf[i] == 't'){
+				for(j=0;j<sizeof(tempBuf);j++){
+					tempBuf[j] = buf[i+2+j];
+				}}}
+
+		temp = (float)atoi(tempBuf) /1000 ;
+            printf("%.3f C\n",temp);
+		close(fd);
+
+
+
+    lcdPosition(lcd, 10, 0);
+    lcdPrintf(lcd, "%.1f C", temp);
+
+    if(temp<=t3){
+    digitalWrite(LedPin0, LOW); //led on
+            delay(100);
+    }
+
+    else if(temp>t3){
+
+    digitalWrite(LedPin0, HIGH); //led off
+    delay(100);
+    }
+
+    if(txi == 1){
+   //cas
+   seconds = time(NULL);
+    printf("aktualne sekundy: %ld\n", seconds);
+    nowtime=seconds;
+
+    dlzkavaru=nowtime-vysladzStarttime;
+
+
+
+  inputSecond=dlzkavaru;
+
+  hours1 = (inputSecond/secondsInHour);
+
+  remainingSeconds = inputSecond - (hours1 * secondsInHour);
+  minutes1 = remainingSeconds/secondsInMinute;
+
+  remainingSeconds = remainingSeconds - (minutes1*secondsInMinute);
+  seconds1 = remainingSeconds;
+  dlzkavaru=dlzkavaru/60;
+
+printf("\n %.3f \n", dlzkavaru);
+printf("\n %d \n", txHlas);
+  if (dlzkavaru>=txHlas){
+
+    lcdPosition(lcd, 0, 1);
+    lcdPrintf(lcd, "Vysladz. voda");
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+sleep(1);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+delay(100);
+digitalWrite(BuzzPin, 1); //led on
+delay(100);
+digitalWrite(BuzzPin, 0); //led on
+
+txi=0;
+
+
+  }
+}
+
+
+
+   //cas
+   seconds = time(NULL);
+    printf("aktualne sekundy: %ld\n", seconds);
+    nowtime=seconds;
+
+    dlzkavaru=nowtime-etapaStarttime;
+
+
+  inputSecond=dlzkavaru;
+
+  hours1 = (inputSecond/secondsInHour);
+
+  remainingSeconds = inputSecond - (hours1 * secondsInHour);
+  minutes1 = remainingSeconds/secondsInMinute;
+
+  remainingSeconds = remainingSeconds - (minutes1*secondsInMinute);
+  seconds1 = remainingSeconds;
+
+  printf("cas od zaciatku t1:  %d:%d:%d\n",hours1,minutes1,seconds1);
+
+
+
+lcdPosition(lcd, 0, 0);
+lcdPrintf(lcd, "%d:%d:%d",hours1,minutes1,seconds1);
+
+//dlzkavaru v minutach
+dlzkavaru=dlzkavaru/60;
+
+if (dlzkavaru>=c3){
+	x=0;
+	}
+
+
+
+} //koniec while 7
+
+} //koniec programu
